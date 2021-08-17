b018d7e718fedc595b93a525f6f190499b86e192
Lê Huy <48431903+lehuylxag1995@users.noreply.github.com>
2021-08-17 15:16:47 +0700

    Read Database

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,12 +1,19 @@
-const course = require('../models/Course')
+const Course = require('../models/Course')
 
 class HomeController {
   // GET /
-  index(req, res) {
-    course.find({}, function (err, course) {
-      if (!err) res.json(course)
-      else res.status(500).json({ err: 'lỗi' })
-    })
+  index(req, res, next) {
+    // course.find({}, function (err, course) {
+    //   if (!err) res.json(course)
+    //   else res.status(500).json({ err: 'lỗi' })
+    // })
+
+    Course.find({})
+      .then((courses) => {
+        courses = courses.map((khoahoc) => khoahoc.toObject())
+        res.render('home', { courses })
+      })
+      .catch(next)
   }
 }
 
