5b4721b4b49a8ed6cefb410b9a0b86e35c0a1664
Lê Huy <48431903+lehuylxag1995@users.noreply.github.com>
2021-08-21 14:16:16 +0700

    update count documents deleted in trash

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,13 +2,26 @@ const Course = require('../models/Course')
 
 class MeController {
   // GET /me/course/list
-  listCourse(req, res, next) {
-    Course.find({})
-      .lean()
-      .then((courses) => {
-        res.render('me/list-course', { courses })
-      })
-      .catch(next)
+  // listCourse(req, res, next) {
+  //   Promise.all([Course.find({}).lean(), Course.countDocumentsDeleted().lean()])
+  //     .then(([courses, countDocumentDeleted]) => {
+  //       res.render('me/list-course', {
+  //         courses,
+  //         countDocumentDeleted
+  //       })
+  //     })
+  //     .catch(next)
+  // }
+
+  // GET /me/course/list
+  async listCourse(req, res, next) {
+    try {
+      const courses = await Course.find({}).lean()
+      const countDocumentDeleted = await Course.countDocumentsDeleted().lean()
+      return res.render('me/list-course', { courses, countDocumentDeleted })
+    } catch (error) {
+      this.listCourse.catch(next)
+    }
   }
 
   // GET /me/course/trash
